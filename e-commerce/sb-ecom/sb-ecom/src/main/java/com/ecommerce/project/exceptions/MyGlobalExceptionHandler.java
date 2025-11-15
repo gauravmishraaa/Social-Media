f848d3d9ac3e9219d0ddcd076a7beefc4d629751
Gaurav Mishra <gauravmishra271865@gmail.com>
2025-11-16 02:50:08 +0530

    pagination

--- e-commerce/sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ e-commerce/sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.ecommerce.project.exceptions;
 
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,29 +12,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-
 public class MyGlobalExceptionHandler {
-    @ExceptionHandler( MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e ){
-     Map<String,String> response= new HashMap<>();
-     e.getBindingResult().getAllErrors().forEach((err)->{
-         String fieldName = ((FieldError)err).getField();
-         String message =  err.getDefaultMessage();
-         response.put(fieldName, message);
-     });
 
-     return   new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+        return new ResponseEntity<Map<String, String>>(response,
+                HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
-    String message = e.getMessage();
-    return  new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException e){
+    public ResponseEntity<String> myAPIException(APIException e) {
         String message = e.getMessage();
-        return  new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
